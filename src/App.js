0fe4485bca3793adef23b0f68fcf217e880fb550
Sheshant-Manure <msheshant1997@gmail.com>
2023-10-09 03:48:18 +0530

    Update album in the list

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ function App() {
   // Declare the albums state variable and a function to update it
   const [albums, setAlbums] = useState([]);
 
+  // Creating refs to get input values
   const userIdRef = useRef(null);
+  const albumIdRef = useRef(null);
   const titleRef = useRef(null);
 
+  // Adding a new album
   const addAlbum = (e) => {
     e.preventDefault();
+
+    // Getting the values from input fields
     const userId = userIdRef.current.value;
     const title = titleRef.current.value;
 
@@ -29,8 +34,47 @@ function App() {
     })
     .then((response) => response.json())
     .then((json) => console.log(json));
+
+    // clearing the form after updating the state and server with a new album
+    userIdRef.current.value = '';
+    titleRef.current.value = '';
+
   }
 
+  // Updating existing album (changing the title)
+  const updateAlbum = (e) => {
+    e.preventDefault();
+    // Getting the values from input fields
+    const id = +albumIdRef.current.value;
+    const title = titleRef.current.value;
+    const userId = +userIdRef.current.value;
+
+    // Updating the state
+    const updatedAlbum = { userId, id, title }
+    setAlbums((prevAlbums) => {
+      return prevAlbums.map((album) => {
+        if (album.id === updatedAlbum.id && album.userId === updatedAlbum.userId) {
+          return updatedAlbum;
+        } else {
+          return album;
+        }
+      })
+    });
+
+    // Updating existing album in the server using API PUT request
+    fetch(`https://jsonplaceholder.typicode.com/albums/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(
+      updatedAlbum
+    ),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+      
+  }
   useEffect(() => {
     // Fetching data from API using GET request method (since no data is sent from the client)
     const fetchAPI = async () => {
@@ -43,7 +87,6 @@ function App() {
           const data = await APIrequest.json();
           // Update the 'albums' state with the fetched data
           setAlbums(data);
-          console.log(data);
         }
       } catch (error) {
         console.log('Something went wrong! The API requested returned an error:', error);
@@ -54,6 +97,7 @@ function App() {
 
   return (
     <>
+      <h1 id='title'>Albums List React App</h1>
       <div className='forms-container'>
         <div className='add-album'>
           <h2>Add an album</h2>
@@ -65,6 +109,12 @@ function App() {
         </div>
         <div className='update-album'>
           <h2>Update an album</h2>
+          <form onSubmit={updateAlbum}>
+            <input ref={userIdRef} type='text' placeholder='User Id' required /><br />
+            <input ref={albumIdRef} type='text' placeholder='Album Id' required /><br />
+            <input ref={titleRef} type='text' placeholder='New Title' required /><br />
+            <button type='submit'><b>Update Album</b></button>
+          </form>
         </div>
         <div className='delete-album'>
           <h2>Delete an album</h2>
