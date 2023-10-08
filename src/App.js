58d3ffb8a2bd8f4bb09beb22276795c7cd249be9
Sheshant-Manure <msheshant1997@gmail.com>
2023-10-09 02:02:39 +0530

    Add album to the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './Assets/style.css';
 
 function App() {
   // Declare the albums state variable and a function to update it
   const [albums, setAlbums] = useState([]);
+
+  const userIdRef = useRef(null);
+  const titleRef = useRef(null);
+
+  const addAlbum = (e) => {
+    e.preventDefault();
+    const userId = userIdRef.current.value;
+    const title = titleRef.current.value;
+
+    // Defining the album object to update the state and send to the server
+    const album = { id: albums.length, userId, title }
+    setAlbums([...albums, album])
+
+    // Adding a new album to the server using API POST request
+    fetch('https://jsonplaceholder.typicode.com/albums', {
+    method: 'POST',
+    body: JSON.stringify(
+      album
+    ),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }
 
   useEffect(() => {
     // Fetching data from API using GET request method (since no data is sent from the client)
@@ -28,11 +54,28 @@ function App() {
 
   return (
     <>
+      <div className='forms-container'>
+        <div className='add-album'>
+          <h2>Add an album</h2>
+          <form onSubmit={addAlbum}>
+            <input ref={userIdRef} type='text' placeholder='User Id' required /><br />
+            <input ref={titleRef} type='text' placeholder='Title' required /><br />
+            <button type='submit'><b>Add Album</b></button>
+          </form>
+        </div>
+        <div className='update-album'>
+          <h2>Update an album</h2>
+        </div>
+        <div className='delete-album'>
+          <h2>Delete an album</h2>
+        </div>
+      </div>
+      {/* Showing all the Albums in a flex-box container */}
       <div className='container'>
         <ol>
-          {albums.map((album)=>(
-            <div className='album-box'>
-              <li key={album.id}>{album.title}</li>
+          {albums.map((album, i)=>(
+            <div key={i} className='album-box'>
+              <li>{album.title}</li>
             </div>
           ))}
         </ol>
